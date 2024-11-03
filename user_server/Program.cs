1c02e861e3e62402217bf2188a44eb6c9b91d9c4
passioncitizen <passioncitizen@gmail.com>
2024-11-03 05:36:01 +0000

    #34. 테스트코드 원복

--- user_server/Program.cs
+++ user_server/Program.cs
@@ -1,4 +1,9 @@
-﻿using network.common.helpers;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using network.core;
+using network.infrastructure;
 using network.managers;
 
 namespace user_server;
@@ -9,30 +14,28 @@ public static class Program
 
     public static async Task Main(string[] args)
     {
-        // await Host.CreateDefaultBuilder(args)
-        //     .ConfigureAppConfiguration((_, config) => { config.AddEnvironmentVariables(); })
-        //     .ConfigureLogging((hostingContext, logging) =>
-        //     {
-        //         logging.ClearProviders();
-        //         logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
-        //         logging.AddConsole();
-        //     })
-        //     .ConfigureServices((_, services) =>
-        //     {
-        //         services.AddSingleton<NetworkService>();
-        //         services.AddSingleton<RedisConnectionPool>();
-        //         services.AddSingleton<NatsClientFactory>();
-        //         services.AddSingleton(sp =>
-        //         {
-        //             var config = sp.GetRequiredService<IConfiguration>();
-        //             var serverType = config["ServerType"] ?? "UserServer";
-        //             GameServerNum = config.GetValue<int>("GameServerNum");
-        //             return new LogManager(serverType, 0);
-        //         });
-        //         services.AddHostedService<UserServer>();
-        //     })
-        //     .RunConsoleAsync();
-
-        GameDataHelper.Initialize(new LogManager("", 0));
+        await Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((_, config) => { config.AddEnvironmentVariables(); })
+            .ConfigureLogging((hostingContext, logging) =>
+            {
+                logging.ClearProviders();
+                logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                logging.AddConsole();
+            })
+            .ConfigureServices((_, services) =>
+            {
+                services.AddSingleton<NetworkService>();
+                services.AddSingleton<RedisConnectionPool>();
+                services.AddSingleton<NatsClientFactory>();
+                services.AddSingleton(sp =>
+                {
+                    var config = sp.GetRequiredService<IConfiguration>();
+                    var serverType = config["ServerType"] ?? "UserServer";
+                    GameServerNum = config.GetValue<int>("GameServerNum");
+                    return new LogManager(serverType, 0);
+                });
+                services.AddHostedService<UserServer>();
+            })
+            .RunConsoleAsync();
     }
 }
