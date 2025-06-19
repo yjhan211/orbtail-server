668edb1c675bec6242c537e335e792daba283a10
passioncitizen <passioncitizen@gmail.com>
2025-06-19 11:45:21 +0900

    #64. csv 수정

--- user_server/Players/PlayerCraft.cs
+++ user_server/Players/PlayerCraft.cs
@@ -150,24 +150,24 @@ public class PlayerCraft(GameUser user, PlayerInfo playerInfo, PlayerQuest playe
                 var generationQueue = new List<int>();
                 switch (itemDetail.Id)
                 {
-                    case 301000013: // 소형 택배 상자 - 소형 택배 상자
-                        generationQueue = [11, 11, 11];
+                    case 301000013: // 소형 택배 상자 - 감자 한 박스
+                        generationQueue = [11, 11, 11, 11, 11];
                         break;
                     
                     case 301000012: // 의자 - 첵가방
-                        generationQueue = [11, 11];
+                        generationQueue = [51, 51, 51, 51];
                         break;
                     
                     case 301000014: // 책상 - 필통
-                        generationQueue = [41];
+                        generationQueue = [41, 61, 61, 61, 61];
                         break;
 
                     case 301000015: // 2층 교실 옆 상자 - 밤양갱
-                        generationQueue = [31, 31, 31, 31, 21, 21, 21, 21, 51, 51, 51, 51];
+                        generationQueue = [31, 31, 31, 31];
                         break;
                     
                     case 301000016: 
-                        generationQueue = [21, 21, 21, 21, 21, 21, 21, 21, 41, 41, 61, 61, 61, 61];
+                        generationQueue = [41, 41];
                         break;
                     
                     case 301000017: // 휴지통 - 검정 비닐 봉지
